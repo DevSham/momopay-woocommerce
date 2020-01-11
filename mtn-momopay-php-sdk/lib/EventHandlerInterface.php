aa997b396fe5e9a85603ce365353a80cbb020517
allandereal <ahullan@gmail.com>
2020-01-11 17:28:53 +0300

    add event handler methods and their functionality

---
+++ mtn-momopay-php-sdk/lib/EventHandlerInterface.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allan
+ * Date: 1/11/20
+ * Time: 1:33 PM
+ */
+
+namespace MTN;
+
+
+interface EventHandlerInterface
+{
+    function onInit();
+
+    function onAccessTokenFailure();
+
+    function onPaymentRequestInit($reference_id, $external_id);
+
+    function onPaymentRequestFailure();
+
+    function onPaymentRequestSuccess();
+
+    function onPaymentRequestStatusCheck($reference_id);
+
+    function onPaymentRequestStatusCheckFailure($reference_id);
+
+    function onSuccessful($transaction_data, $env, $reference_id);
+
+    function onFailure($reference_id);
+
+    function onRequery($reference_id);
+
+    function onCancel();
+
+    function onTimeout($reference_id);
+
+    function onReject($reference_id);
+}
