99ceaa4d672add5b4ed89024ff06538ec0f7334a
allandereal <ahullan@gmail.com>
2020-01-11 20:00:55 +0300

    fix auto phone update at checkout

--- assets/js/momopay.js
+++ assets/js/momopay.js
@@ -1,11 +1,25 @@
-window.onload = function () {
-    var momopay_currencies = momopay_args.currencies;
-    var momopay_go_live = momopay_args.go_live;
+var formPhoneField;
+var formCountryField;
+var phoneField;
+var currencyField;
 
-    var formPhoneField = document.getElementById('billing_phone');
-    var formCountryField = document.getElementById('billing_country');
-    var phoneField = document.getElementById('momopay_phone');
-    var currencyField = document.getElementById('momopay_currency');
+var sanitizedPhone = function (phone) {
+    phone = phone.trim();
+    if (phone.charAt(0) === '+'){
+        phone = phone.substring(4, phone.length);
+        return '0' + phone.trim();
+    }
+    return phone;
+};
+
+window.onload = function () {
+    momopay_currencies = momopay_args.currencies;
+    momopay_go_live = momopay_args.go_live;
+
+    formPhoneField = document.getElementById('billing_phone');
+    formCountryField = document.getElementById('billing_country');
+    phoneField = document.getElementById('momopay_phone');
+    currencyField = document.getElementById('momopay_currency');
 
     //initialize fields
     phoneField.value = formPhoneField.value;
@@ -20,13 +34,4 @@ window.onload = function () {
     formPhoneField.addEventListener('change', function (e) {
         phoneField.value = sanitizedPhone(this.value);
     });
-
-    var sanitizedPhone = function (phone) {
-        phone = phone.trim();
-        if (phone.charAt(0) === '+'){
-            phone = phone.substring(4, phone.length);
-            return '0' + phone.trim();
-        }
-        return phone;
-    };
 };
